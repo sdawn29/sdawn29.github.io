287b92a104be3d10214e8083c0929ce38dec138a
sdawn29 <snehasishdawn29@gmail.com>
2020-04-28 15:31:58 +0530

    Add files via upload

---
+++ precache-manifest.e500cd02fd375ae4ac35183bdf2f3ec2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "51629e0eaee93577f344",
+    "url": "/css/app.b62ee54c.css"
+  },
+  {
+    "revision": "5651245babb29b68dc09f58af2e47dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6db38d85a48cc5787f",
+    "url": "/js/about.7abd82d2.js"
+  },
+  {
+    "revision": "51629e0eaee93577f344",
+    "url": "/js/app.66ac6327.js"
+  },
+  {
+    "revision": "fb122b4681b4e06bb15d",
+    "url": "/js/chunk-2d2086b7.1d3dc9fd.js"
+  },
+  {
+    "revision": "4443e9b5a1ff87a2c874",
+    "url": "/js/chunk-vendors.735d88dc.js"
+  },
+  {
+    "revision": "da401b94bd7de87d74c6f22339415993",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
